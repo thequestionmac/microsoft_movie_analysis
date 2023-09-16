1ecf95fe9979c993752d52a60e0bacbd860df167
Mark Njagi <marknjagi766@gmail.com>
2023-09-16 23:32:01 +0300

    Update README.md

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# microsoft_movie_analysis
+#### The Return of Microsoft into the Movie Industry <br>
+
+__About__: This project aims at providing insight to Microsoft's Head of Studio to enable them make informed decision pending their decision
+to relaunch the Xbox Entertainment Studios. <br>
+
+__Contributors__: Mark Njagi <br>
+
+__Contributing__: Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change
+
+
+
--- code/datacleaning/remove_duplicates.py
+++ code/datacleaning/remove_duplicates.py
@@ -1,14 +1,14 @@
-def remove_duplicates(data):
+def remove_duplicates(df):
     '''
     Checks for, displays and drops all duplicate values in the dataset
     '''
-    number_of_duplicates = data.duplicated().sum()
+    number_of_duplicates = df.duplicated().sum()
     
     if number_of_duplicates > 0:
         print (f'This dataset has {number_of_duplicates} duplicated values')
         print ('Below is a sample of duplicates: ')
-        print (data.iloc[df.duplicated()].sort_values(by = 'title').head(10))
-        data.drop_duplicated(inplace = True)
+        print (df[df.duplicated()].sort_values(by = 'title').head(10))
+        df.drop_duplicates(inplace = True)
         print ('Duplicate values removed')
     else:
         print ('No duplicate values')
